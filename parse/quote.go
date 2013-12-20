66f1c43b8d6fce1d4107cfa3dca68c6ea10ffbd1
Rob Figueiredo <robfig@gmail.com>
2013-12-20 09:39:51 -1000

    add quoting and stringnode test

---
+++ parse/quote.go
@@ -0,0 +1,96 @@
+package parse
+
+import (
+	"errors"
+	"strconv"
+	"unicode/utf8"
+)
+
+var unescapes = map[rune]rune{
+	'\\': '\\',
+	'\'': '\'',
+	'n':  '\n',
+	'r':  '\r',
+	't':  '\t',
+	'b':  '\b',
+	'f':  '\f',
+}
+
+var escapes = make(map[rune]rune)
+
+func init() {
+	for k, v := range unescapes {
+		escapes[v] = k
+	}
+}
+
+// quoteString quotes the given string with single quotes, according to the Soy
+// spec for string literals.
+func quoteString(s string) string {
+	var q = make([]rune, 1, len(s)+10)
+	q[0] = '\''
+	for _, ch := range s {
+		if seq, ok := escapes[ch]; ok {
+			q = append(q, '\\', seq)
+			continue
+		}
+		q = append(q, ch)
+	}
+	return string(append(q, '\''))
+}
+
+func unquoteString(s string) (string, error) {
+	n := len(s)
+	if n < 2 {
+		return "", errors.New("too short a string")
+	}
+
+	if '\'' != s[0] || '\'' != s[n-1] {
+		return "", errors.New("string not surrounded by quotes")
+	}
+
+	s = s[1 : n-1]
+	if !contains(s, '\\') && !contains(s, '\'') {
+		return s, nil
+	}
+
+	var escaping = false
+	var result = make([]rune, 0, len(s))
+	for i := 0; i < len(s); {
+		r, size := utf8.DecodeRuneInString(s[i:])
+		i += size
+
+		if escaping {
+			if r == 'u' {
+				if i+5 <= len(s) {
+					return "", errors.New("error scanning unicode escape, expect \\u[0-9]{4}")
+				}
+				num, err := strconv.ParseInt(s[i+1:i+5], 16, 0)
+				if err != nil {
+					return "", err
+				}
+				r = rune(num)
+				i += 4
+			} else {
+				replacement, ok := unescapes[r]
+				if !ok {
+					return "", errors.New("unrecognized escape code: \\" + s[i:i+1])
+				}
+				r = rune(replacement)
+			}
+		}
+
+		result = append(result, r)
+		escaping = r == '\\'
+	}
+	return string(result), nil
+}
+
+func contains(s string, c byte) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] == c {
+			return true
+		}
+	}
+	return false
+}
