536e0f236da20dcb617c6f5101bf99e8dd9bf8fd
ttacon <ttacon@gmail.com>
2015-01-17 09:10:14 -0500

    Add data.Marshaler

--- data/convert_test.go
+++ data/convert_test.go
@@ -50,6 +50,10 @@ func TestNew(t *testing.T) {
 			PI *AInt
 		}{{nil}},
 			List{Map{"pI": Null{}}}},
+		{testIDURL{1, "https://github.com/robfig/soy"},
+			Map{"iD": Int(1), "uRL": String("https://github.com/robfig/soy")}},
+		{testIDURLMarshaler{1, "https://github.com/robfig/soy"},
+			Map{"id": Int(1), "url": String("https://github.com/robfig/soy")}},
 	}
 
 	for _, test := range tests {
@@ -57,6 +61,20 @@ func TestNew(t *testing.T) {
 		if !reflect.DeepEqual(test.expected, output) {
 			t.Errorf("%#v =>\n %#v, expected:\n%#v", test.input, output, test.expected)
 		}
+	}
+}
+
+type testIDURL struct {
+	ID  int
+	URL string
+}
+
+type testIDURLMarshaler testIDURL
+
+func (t testIDURLMarshaler) MarshalValue() Value {
+	return Map{
+		"id":  New(t.ID),
+		"url": New(t.URL),
 	}
 }
 
